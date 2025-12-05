ca3e33089f11ccb71faef950ab77dd86121b56a0
AlexRoa6 <alexroasebastian9@gmail.com>
2025-12-05 20:19:56 +0100

    Diseño basico de la Homepage hecho

---
+++ app/src/main/java/com/example/checkit/model/Task.kt
@@ -0,0 +1,12 @@
+package com.example.checkit.model
+
+import java.time.LocalDate
+import java.util.Date
+
+data class Task (
+    val id: Int,
+    val title: String,
+    val description: String = "",
+    val completed: Boolean = false,
+    val date: LocalDate
+)
--- app/src/main/java/com/example/checkit/ui/theme/Theme.kt
+++ app/src/main/java/com/example/checkit/ui/theme/Theme.kt
@@ -29,20 +29,9 @@ private val DarkColorScheme = darkColorScheme(
 
 @Composable
 fun CheckItTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = DarkColorScheme
 
     MaterialTheme(
         colorScheme = colorScheme,
