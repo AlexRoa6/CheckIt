dd6369238fbdf0d08e3f26ecdb8dbf45767a718c
AlexRoa6 <alexroasebastian9@gmail.com>
2025-12-09 08:43:33 +0100

    Arreglado el problema de que no se cerraba el calendario

--- app/src/main/java/com/example/checkit/view/NewtaskFormView.kt
+++ app/src/main/java/com/example/checkit/view/NewtaskFormView.kt
@@ -6,9 +6,6 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,7 +15,6 @@ import com.example.checkit.ui.theme.CheckItTheme
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -30,20 +26,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDatePickerState
-import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
@@ -60,7 +52,6 @@ import com.example.checkit.ui.theme.Primary
 import com.example.checkit.ui.theme.Shapes
 import com.example.checkit.viewModel.NewTaskUiState
 import com.example.checkit.viewModel.NewTaskViewModel
-import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
@@ -122,6 +113,7 @@ fun Form(
                 showDatePicker = uiState.isDatePickerVisible,
                 onDatePickerVisibilityChange = { showDatePicker(true) },
                 onDateSelected = onDateSelected,
+                onDismiss = { showDatePicker(false) }
             )
             Spacer(Modifier.height(24.dp))
             TaskPriority(uiState.priority, onPrioritySelected)
@@ -211,6 +203,7 @@ fun DueDate(
     showDatePicker: Boolean,
     onDateSelected: (Long) -> Unit,
     onDatePickerVisibilityChange: (Boolean) -> Unit,
+    onDismiss: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     val datePickerState = rememberDatePickerState(
@@ -234,14 +227,14 @@ fun DueDate(
                 ),
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            Text(
-                text = DateTimeFormatter.ofPattern("dd/MM/yyyy").format(date),
-                style = MaterialTheme.typography.titleMedium,
-                modifier = Modifier.weight(1f)
-            )
             Icon(
                 painter = painterResource(R.drawable.calendar_month),
                 contentDescription = null
+            )
+            Spacer(Modifier.width(8.dp))
+            Text(
+                text = DateTimeFormatter.ofPattern("dd/MM/yyyy").format(date),
+                style = MaterialTheme.typography.titleMedium,
             )
         }
     }
@@ -257,7 +250,7 @@ fun DueDate(
                 }
             },
             dismissButton = {
-                Button(onClick = { onDatePickerVisibilityChange(false) }) {
+                Button(onClick = { onDismiss() }) {
                     Text(stringResource(R.string.cancelar))
                 }
             },
