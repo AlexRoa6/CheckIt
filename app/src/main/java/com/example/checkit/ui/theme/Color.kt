cd154592e4fa19d5acf0a0c7d02ea983d5d9cd35
AlexRoa6 <alexroasebastian9@gmail.com>
2026-01-20 13:19:02 +0100

    Mejora de estilo y colores

--- app/src/main/java/com/example/checkit/ui/theme/Color.kt
+++ app/src/main/java/com/example/checkit/ui/theme/Color.kt
@@ -3,16 +3,26 @@ package com.example.checkit.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // PRIMARY
-val Primary = Color(0xFF13A4EC)
+val Primary = Color(0xFF2E5BFF)
 
 // BACKGROUNDS
-val BackgroundLight = Color(0xFFF6F7F8)
-val BackgroundDark = Color(0xFF101C22)
+val BackgroundLight = Color(0xFFEFF6FF)
+val BackgroundDark = Color(0xFF0A0A0A)
 
 // CARDS
 val CardLight = Color(0xFFFFFFFF)
 val CardDark = Color(0xFF132128)
+val CardBorderLight = Color(0xFFF6F3F3)
+val CardBorderDark = Color(0xFF262626)
 
+val CardCheckedBackGroundLight = Color(0xFFD3DCE7)
+val CardCheckedBackGroundDark = Color(0xFF0E0E0E)
+
+val BorderCardCheckedBackGroundLight = Color(0x33D4D4D5)
+val BorderCardCheckedBackGroundDark = Color(0x331D1D1D)
+
+val CheckedTextLight = Color(0x332C2E38)
+val CheckedTextDark = Color(0x33A4A4A8)
 // TEXT
 val TextPrimaryLight = Color(0xFF111827)
 val TextPrimaryDark = Color(0xFFFFFFFF)
@@ -37,8 +47,8 @@ val DividerLight = Gray200
 val DividerDark = Color(0x1F293780) // gray-800/50
 
 // ICON BACKGROUND
-val IconBgLight = Color(0xFFE0F4FF)   // primary /10
-val IconBgDark = Color(0xFF233C47)
+val IconBgLight = Color(0xFFFFFFFF)   // primary /10
+val IconBgDark = Color(0xFF171717)
 
 // CHECKBOX BORDER (dark)
 val DarkBorderCheckbox = Color(0xFF325567)
@@ -49,7 +59,7 @@ val PriorityHighBgLight = Color(0x1AEF4444) // red-500/10
 val PriorityHighBgDark = Color(0x33EF4444)  // red-500/20
 
 // PRIORITY MEDIUM (yellow-500)
-val PriorityMedium = Color(0xFFEAB308)
+val PriorityMedium = Color(0xFFFFFA00)
 val PriorityMediumBgLight = Color(0x1AEAB308) // yellow-500/10
 val PriorityMediumBgDark = Color(0x33EAB308)  // yellow-500/20
 
@@ -60,4 +70,4 @@ val PriorityLowBgDark = Color(0x3322C55E)  // green-500/20
 
 // FOOTER BUTTON
 val FooterDeleteBgLight = Color(0x1AEF4444)
-val FooterDeleteBgDark = Color(0x6FEF4444)
+val FooterDeleteBgDark = Color(0xBFF14444)
--- app/src/main/java/com/example/checkit/ui/theme/Shape.kt
+++ app/src/main/java/com/example/checkit/ui/theme/Shape.kt
@@ -7,7 +7,7 @@ import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
     small = RoundedCornerShape(4.dp),    // 0.25rem -> botones pequeños, checkboxes
-    medium = RoundedCornerShape(8.dp),   // 0.5rem -> tarjetas, listas
-    large = RoundedCornerShape(12.dp),   // 0.75rem -> secciones grandes, contenedores
+    medium = RoundedCornerShape(14.dp),   // 0.5rem -> tarjetas, listas
+    large = RoundedCornerShape(24.dp),   // 0.75rem -> secciones grandes, contenedores
     extraLarge = RoundedCornerShape(9999.dp) // full -> círculos, icon buttons
 )
